eb5f4e88149dc0ff5e7444588e2d323a0cd355f1
Bernard Gütermann <bernard.gutermann@gmail.com>
2013-10-30 10:12:21 +0100

    splitter

--- mnist-ml.py
+++ mnist-ml.py
@@ -1,10 +1,14 @@
 import load_mnist as loader
+import splitter as splitter
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
 (A,B)=loader.loadTrainingSet()
 print 'success'
-imgplot = plt.imshow(A)
-plt.show()
+#imgplot = plt.imshow(A)
+#plt.show()
 #np.reshape(A, A.size)
+(T,V)=splitter.split(A)
+print T.shape,' ',V.shape
