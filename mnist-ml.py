a2052c5b8d74b30a247d950c435eb4ac7a2d33ba
Bernard Gütermann <bernard.gutermann@gmail.com>
2013-10-30 09:36:52 +0100

    loader

--- mnist-ml.py
+++ mnist-ml.py
@@ -1,5 +1,10 @@
 import load_mnist as loader
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
 
 (A,B)=loader.load()
 print 'success'
-
+imgplot = plt.imshow(A)
+plt.show()
+#np.reshape(A, A.size)
