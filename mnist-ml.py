bd752a19a7d1ca0501ebcf35ace86bf47ddd1aec
Bernard Gütermann <bernard.gutermann@gmail.com>
2013-11-03 10:52:21 +0100

    1st part done

--- mnist-ml.py
+++ mnist-ml.py
@@ -2,6 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+#transfer funtion
+def transferF(a1,a2) :
+    return a1*phiF(a2)
+
+#phi
+def phiF(x) :
+    return 1/(1+np.exp(-x))
+
+
 #load data
 T=np.load('mnist/nTrainingSet.npy')
 V=np.load('mnist/nValidationSet.npy')
