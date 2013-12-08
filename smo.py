fad607792b9360c9918c4e03cc970296985685b0
Vincent Robert <vincent.robert@epfl.ch>
2013-12-08 19:31:21 +0100

    SMO seems ok

--- smo.py
+++ smo.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #### AGLG
-tau = 10e-8
+tau = 10e-4
 C = 1
 threshold = 10e-15
 def select_pair(F, I_low, I_up) : 
@@ -20,7 +20,7 @@ def gaussian(A) :
     return np.exp(-tau*A)
     
 def initK(X) :
-    XXT = (X*X.T)
+    XXT = (np.asmatrix(X)*np.asmatrix(X.T))
     d = XXT.diagonal().T
     ones = np.asmatrix(np.ones(d.shape))
     A = 0.5*(d*ones.T) + 0.5*(ones*d.T) - XXT
@@ -29,15 +29,24 @@ def initK(X) :
 #init sets
 def indexSets(Alpha) : 
     indexes = range(0,Alpha.shape[0])
-    I_0 = [i for i in indexes if 0<Alpha[i,0] and Alpha[i,0]<10]
-    I_p = [i for i in indexes if T[i,0]==1 and Alpha[i,0]==0]
-    I_m = [i for i in indexes if T[i,0]==-1 and Alpha[i,0]==0]
+    I_0 = [i for i in indexes if 0<Alpha[i,0] and Alpha[i,0]<C]
+    I_p = [i for i in indexes if (T[i,0]==1 and Alpha[i,0]==0) or (T[i,0]==-1 and Alpha[i,0]==C)]
+    I_m = [i for i in indexes if (T[i,0]==-1 and Alpha[i,0]==0) or (T[i,0]==1 and Alpha[i,0]==C)]
     return (I_0 + I_p, I_0 + I_m)
     
     
 #little xor problem
-X = np.matrix('(0,0);(1,0);(0,1);(1,1)')
-T = np.matrix('-1;1;1;-1')
+#X = np.matrix('(0,0);(1,0);(0,1);(1,1)')
+#T = np.matrix('(-1);(1);(1);(-1)')
+
+#Big training dataset
+X=np.load('mnist/n_MNIST_Training.npy')
+T=np.load('mnist/n_MNIST_Training_labels.npy')
+
+#Big validation dataset
+#X2=np.load('mnist/n_MNIST_Validation.npy')
+#T2=np.load('mnist/n_MNIST_Validation_labels.npy')
+
 #dimensionality
 d = X.shape[1]
 #size of input
@@ -47,29 +56,32 @@ n = X.shape[0]
 F = -T
 K = initK(X)
 Alpha = np.asmatrix(np.zeros((n,1)))
-(I_low, I_up) = indexSets(Alpha)
-print I_low
-print I_up
+(I_up, I_low) = indexSets(Alpha)
+##print I_low
+##print I_up
 #main loop
+#print K
 while 1 :
     (i,j) = select_pair(F, I_low, I_up)
-    print 'i, j :',(i,j)
     if j == -1 :
         break
     sigma = T[i,0]*T[j,0]
     #compute L,H
     w = Alpha[i,0]+sigma*Alpha[j,0]
-    IC = 0 
-    if sigma == 1: IC = C
-    L = max(0,sigma*w-IC)
-    IC = 0 
-    if sigma == -1 : IC = C
-    H = min(C, sigma*w + IC)
+    IL = 0
+    IH = 0 
+    if sigma == 1: 
+        IL = C
+    else:
+        IH = C
+    L = max(0,w-IL)
+    H = min(C, w + IH)
+   # print "sigma, L,H,w,IL,IH : ",sigma,L,H,w,IL,IH
     alpha_new_j = 0
     rho = K[i,i] + K[j,j] - 2*K[i,j]
     if rho > threshold :
         alpha_unc = Alpha[j,0] + (T[j,0]*(F[i,0]-F[j,0])/rho)
-        if alpha_unc in range(L,H) : 
+        if alpha_unc >= L and alpha_unc <= H : 
            alpha_new_j = alpha_unc
         if alpha_unc < L :
            alpha_new_j = L
@@ -81,17 +93,18 @@ while 1 :
         #partie insupportable
         v_i = F[i,0] + T[i,0] - Alpha[i,0]*T[i,0]*K[i,i] - Alpha[j,0]*T[j,0]*K[i,j]
         v_j = F[j,0] + T[j,0] - Alpha[i,0]*T[i,0]*K[i,j] - Alpha[j,0]*T[j,0]*K[j,j]
-        phi_L = 0.5*(K[i,i]*L_i*L_i+K[j,j]*L*L)+sigma*K[i,j]*L_i*L+T[i,0]*L_i*v_i + T[j,0]*L*v_j - L_i - L #formule insupportable
-        phi_H = 0.5*(K[i,i]*H_i*H_i+K[j,j]*H*H)+sigma*K[i,j]*H_i*H+T[i,0]*H_i*v_i + T[j,0]*H*v_j - H_i - H #formule insupportable
+        phi_L = 0.5*(K[i,i]*L_i*L_i+K[j,j]*L*L)+sigma*K[i,j]*L_i*L+T[i,0]*L_i*v_i + T[j,0]*L*v_j - L_i - L
+        phi_H = 0.5*(K[i,i]*H_i*H_i+K[j,j]*H*H)+sigma*K[i,j]*H_i*H+T[i,0]*H_i*v_i + T[j,0]*H*v_j - H_i - H
         if phi_L > phi_H :
             alpha_new_j = H
         else :
-            alpha_new_j
+            alpha_new_j = L
     alpha_new_i = Alpha[i,0] + sigma*(Alpha[j,0] - alpha_new_j)
     F = np.add(F,np.add(T[i,0]*(alpha_new_i-Alpha[i,0])*K[:,i],T[j,0]*(alpha_new_j - Alpha[i,0])*K[:,j]))
-    print "F : ",F
+    #print "F : ",F
     Alpha[i,0] = alpha_new_i
     Alpha[j,0] = alpha_new_j
-    (I_low, I_up) = indexSets(Alpha)
-print "F final : ",F
+    (I_up, I_low) = indexSets(Alpha)
+print F
+print Alpha
     
