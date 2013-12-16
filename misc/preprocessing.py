f143b24fd595cec1ef92857cadbdad635c75e99a
Bernard Gütermann <bernard.gutermann@gmail.com>
2013-12-16 15:13:38 +0100

    preprocessing

--- misc/preprocessing.py
+++ misc/preprocessing.py
@@ -50,6 +50,9 @@ print 'dataset 4-9 : success'
 print A.shape
 #normalize
 N, tmax, tmin = normalize(A)
+#save complete set (for SMO)
+np.save('../mnist/n_MNIST_Complete49',N)
+np.save('../mnist/n_MNIST_Complete49_Label',L)
 #split data
 (T, T_l, V, V_l)=splitter.split(N, L)
 print '4-9 : Training.training : ', T.shape, ' l : ',T_l.shape, ' ; Training.validation : ',V.shape, ' l : ', ' max :',tmax,' min', tmin 
@@ -68,30 +71,9 @@ N,_,__ = normalize(A, tmax, tmin)
 #split data
 print '4-9 :Test set : ', N.shape, ' l : ',L.shape,' max :',tmax,' min', tmin
 #save to disk
-
 np.save('../mnist/n_MNIST_Test49',N)
 np.save('../mnist/n_MNIST_Test_labels49',L)
 
-#Complete 4-9 dataset
-#Normalize dataset
-def normalize(A):
-    #min max
-    amax = np.amax(A)
-    amin = np.amin(A)
-    #normalize
-    J = amin*np.ones(A.shape)
-    N = (A-J)/(amax-amin)
-    return N
-    
-#SAME for second dataset
-#load data
-(A,L)=loader.loadTrainingSet_49()
-print 'dataset 4-9 : success'
-print A.shape
-#normalize
-N = normalize(A)
-print '4-9 : Complete : ', N.shape,L.shape
-#save to disk
-np.save('../mnist/n_MNIST_Complete49',N)
-np.save('../mnist/n_MNIST_Complete49_Label',L)
+
+
 
